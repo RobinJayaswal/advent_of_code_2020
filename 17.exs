174797c47b74300fb49ef0b51a7d4d504c8ce18d
Robin Jayaswal <r.e.jayaswal@gmail.com>
2020-12-19 12:52:04 -0800

    comments

--- 17.exs
+++ 17.exs
@@ -21,40 +21,25 @@ defmodule DaySeventeen do
     count_active_cubes(final_state)
   end
 
-  def perform_cycles(state, cycles) when cycles > 0 do
-    updated_state = perform_cycle(state)
-    perform_cycles(updated_state, cycles - 1)
-  end
-
-  def perform_cycles(state, 0) do
-    state
-  end
-
   def count_active_cubes(state) do
     flattened = Array.flatten(state)
     active = Enum.filter(flattened, fn x -> x === "#" end)
     length active
   end
 
+  #=> Recursive: Complete n cycles
+  def perform_cycles(state, cycles) when cycles > 0 do
+    updated_state = perform_cycle(state)
+    perform_cycles(updated_state, cycles - 1)
+  end
+
+  #=> Base case: All cycles completed
+  def perform_cycles(state, 0), do: state
 
 
-  #=> Represent as an array of arrays of arrays
+  #=> State is as an array of arrays of arrays of...
+  #=> Can handle states of any dimensionality
   def perform_cycle(state) do
-    # recursively expand state at each level
-    # sample_layer = Enum.at(state, 0)
-    # sample_row = Enum.at(sample_layer, 0)
-    # zero_layer = Enum.map(1..(length sample_layer), fn _ ->
-    #   Enum.map(1..(length sample_row), fn _ -> "." end)
-    # end)
-    #
-    # expanded_state = Enum.map([zero_layer] ++ state ++ [zero_layer], fn layer ->
-    #   sample_row = Enum.at(layer, 0)
-    #   zero_row = Enum.map(1..(length sample_row), fn _ -> "." end)
-    #   Enum.map([zero_row] ++ layer ++ [zero_row], fn row ->
-    #     ["."] ++ row ++ ["."]
-    #   end)
-    # end)
-
     expanded_state = create_expanded_state(state)
 
     Enum.map(
@@ -63,6 +48,7 @@ defmodule DaySeventeen do
     )
   end
 
+  #=> Recursive expand the state by 1 in every direction
   def create_expanded_state(state) when is_list(state) do
     sample_level = Enum.at(state, 0)
     inactive_level = create_level_of_inactives(sample_level)
@@ -70,19 +56,19 @@ defmodule DaySeventeen do
     Enum.map([inactive_level] ++ state ++ [inactive_level], &create_expanded_state(&1))
   end
 
+  #=> Base case: Reached non-list level in state, aka bottom
   def create_expanded_state(value), do: value
 
+  #=> Recursive: Create an object with same shape as level, but all inactive
   def create_level_of_inactives(level) when is_list(level) do
     Enum.map(level, &create_level_of_inactives(&1))
   end
 
+  #=> Base Case: Reached bottom of state. Return inactive cube
   def create_level_of_inactives(_level), do: "."
 
-  #=> We'll rewrite all of these updates into one single one that's recursive,
-  #=> and the case where we reach a non-array is the base case
-  #=> Then above, we just need to make perform_cycle dimension agnostic, and
-  #=> have part 2 pass in an object with an extra dimension
-
+  #=> Recursive: Apply updates to a level of state. Do this by applying updates
+  #=> to each element of this level of state
   def update_level(
     { level, level_coordinate }, other_coordinates, original_state
   ) when is_list(level) do
@@ -92,6 +78,8 @@ defmodule DaySeventeen do
     )
   end
 
+  #=> Base Case: Reached bottom level of state, where level is just a value.
+  #=> Update this value based on the rules
   def update_level(
     { level, level_coordinate }, other_coordinates, original_state
   ) do
@@ -132,6 +120,28 @@ defmodule DaySeventeen do
     end
   end
 
+  #=> Recursive: Generate indices of all neighbors for the set of coordinates passed in
+  #=> Do this by generating all neighbors for all coordinates except the first, and
+  #=> combine these with the three possible neighbor values of the first coordinate
+  def generate_neighbor_indices([first_coord | rest_coord], top_level?) do
+    neighbors_on_current_dim = [first_coord - 1, first_coord, first_coord + 1]
+    neighbors_in_rest_of_coords = generate_neighbor_indices(rest_coord, false)
+    neighbors = Enum.flat_map(neighbors_on_current_dim, fn d_coord ->
+      Enum.map(neighbors_in_rest_of_coords, fn neighbor -> [d_coord | neighbor] end)
+    end)
+
+    #=> If top level function call, filter out the original point passed in.
+    case top_level? do
+      true -> Enum.filter(neighbors, fn neighbor -> neighbor != [first_coord | rest_coord] end)
+      false -> neighbors
+    end
+  end
+
+  #=> Base case: No coordinates passed in. The only 'neighbor' is just the null set
+  def generate_neighbor_indices([], _) do
+    [[]]
+  end
+
   def update_active_cube(num_active_neighbors) do
     case num_active_neighbors do
       2 -> "#"
@@ -155,22 +165,6 @@ defmodule DaySeventeen do
     )
   end
 
-  def generate_neighbor_indices([first_coord | rest_coord], top_level?) do
-    neighbors_on_current_dim = [first_coord - 1, first_coord, first_coord + 1]
-    neighbors_in_rest_of_coords = generate_neighbor_indices(rest_coord, false)
-    neighbors = Enum.flat_map(neighbors_on_current_dim, fn d_coord ->
-      Enum.map(neighbors_in_rest_of_coords, fn neighbor -> [d_coord | neighbor] end)
-    end)
-
-    case top_level? do
-      true -> Enum.filter(neighbors, fn neighbor -> neighbor != [first_coord | rest_coord] end)
-      false -> neighbors
-    end
-  end
-
-  def generate_neighbor_indices([], _) do
-    [[]]
-  end
 
   #=> One tricky thing is that you have to catch those outside the existing universe too.
   #=> At the start of every turn, let's add an empty layer in each direction
